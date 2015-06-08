6ca2905fa9239b9428f1268889ff85bbc10aeb2d
love someone <1476390470@qq.com>
2015-06-08 21:51:20 +0800

    test3

---
+++ chats-swift-JsqMessages/JSQMessage-Brigding-Header.m
@@ -0,0 +1,11 @@
+//
+//  JSQMessage-Brigding-Header.m
+//  chats-swift-JsqMessages
+//
+//  Created by kingsun on 15/6/5.
+//  Copyright (c) 2015年 程平. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "JSQSystemSoundPlayer.h"
+#import "JSQMessages.h"
