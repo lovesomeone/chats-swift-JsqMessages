94f21310ae1184fd30fa8e49bacea6886a68947b
cheng ping <1476390470@qq.com>
2015-06-10 16:58:29 +0800

    能看到效果的第一个里程

--- chats-swift-JsqMessages/DemoModelData.swift
+++ chats-swift-JsqMessages/DemoModelData.swift
@@ -19,7 +19,7 @@ let kJSQDemoAvatarIdWoz = "309-41802-93823";
 
 class DemoModelData: NSObject {
     var messages = [JSQMessage]()
-    var avatars = Dictionary<String,UIImage>()
+    var avatars = Dictionary<String,JSQMessagesAvatarImage>()
     var user = Dictionary<String,String>()
     
    
@@ -30,7 +30,9 @@ class DemoModelData: NSObject {
     
     
     override init(){
-        
+        super.init()
+        self.initImage()
+        self.loadFakeMessages();
     }
    // backgroundColor:[UIColor colorWithWhite:0.85f alpha:1.0f]
    // textColor:[UIColor colorWithWhite:0.60f alpha:1.0f]
@@ -42,5 +44,55 @@ class DemoModelData: NSObject {
         
         
         var jsqImage = JSQMessagesAvatarImageFactory.avatarImageWithUserInitials("JQS", backgroundColor:UIColor(white:0.85,alpha:1.0), textColor: UIColor(white:0.85,alpha:1.0), font: UIFont.systemFontOfSize(14.0), diameter: UInt(kJSQMessagesCollectionViewAvatarSizeDefault))
+        
+        var cookImage = JSQMessagesAvatarImageFactory.avatarImageWithImage(UIImage(named: "demo_avatar_cook"), diameter: UInt(kJSQMessagesCollectionViewAvatarSizeDefault))
+        
+        var jobsImage = JSQMessagesAvatarImageFactory.avatarImageWithImage(UIImage(named: "demo_avatar_jobs"), diameter: UInt(kJSQMessagesCollectionViewAvatarSizeDefault))
+        
+        var wozImage = JSQMessagesAvatarImageFactory.avatarImageWithImage(UIImage(named: "demo_avatar_woz"), diameter: UInt(kJSQMessagesCollectionViewAvatarSizeDefault))
+        self.avatars = [kJSQDemoAvatarIdSquires : jsqImage,
+            kJSQDemoAvatarIdCook : cookImage,
+            kJSQDemoAvatarIdJobs : jobsImage,
+            kJSQDemoAvatarIdWoz : wozImage]
+        self.user = [kJSQDemoAvatarIdJobs : kJSQDemoAvatarDisplayNameJobs,
+            kJSQDemoAvatarIdCook : kJSQDemoAvatarDisplayNameCook,
+            kJSQDemoAvatarIdWoz : kJSQDemoAvatarDisplayNameWoz,
+            kJSQDemoAvatarIdSquires : kJSQDemoAvatarDisplayNameSquires]
+        
     }
+    
+    func loadFakeMessages()
+    {
+        var message1 = JSQMessage(senderId: kJSQDemoAvatarIdSquires, senderDisplayName: kJSQDemoAvatarDisplayNameSquires, date: (NSDate.distantPast() as! NSDate), text: "It is simple, elegant, and easy to use. There are super sweet default settings, but you can customize like crazy")
+        
+        var message2 = JSQMessage(senderId: kJSQDemoAvatarIdWoz, senderDisplayName: kJSQDemoAvatarDisplayNameWoz, date: (NSDate.distantPast() as! NSDate), text: "It is simple, elegant, and easy to use. There are super sweet default settings, but you can customize like crazy")
+        
+        var message3 = JSQMessage(senderId: kJSQDemoAvatarIdSquires, senderDisplayName: kJSQDemoAvatarDisplayNameSquires, date: (NSDate.distantPast() as! NSDate), text: "It even has data detectors. You can call me tonight. My cell number is 123-456-7890. My website is www.hexedbits.com")
+        
+        var message4 = JSQMessage(senderId: kJSQDemoAvatarIdJobs, senderDisplayName: kJSQDemoAvatarDisplayNameJobs, date: NSDate(), text: "JSQMessagesViewController is nearly an exact replica of the iOS Messages App. And perhaps, better")
+        
+        var message5 = JSQMessage(senderId: kJSQDemoAvatarIdCook, senderDisplayName: kJSQDemoAvatarDisplayNameCook, date: NSDate(), text: "It is unit-tested, free, open-source, and documented")
+        
+        var message6 = JSQMessage(senderId: kJSQDemoAvatarIdSquires, senderDisplayName: kJSQDemoAvatarDisplayNameSquires, date: NSDate(), text: "Now with media messages!")
+        
+        self.messages.append(message1)
+        self.messages.append(message2)
+        self.messages.append(message3)
+        self.messages.append(message4)
+        self.messages.append(message5)
+        self.messages.append(message6)
+        
+        addPhotoMediaMessage()
+        
+        
+        
+    }
+    
+    func addPhotoMediaMessage()
+    {
+        var photoItem = JSQPhotoMediaItem(image: UIImage(named: "goldengate"))
+        var photoMessage = JSQMessage(senderId: kJSQDemoAvatarIdSquires, displayName: kJSQDemoAvatarDisplayNameSquires, media: photoItem)
+        self.messages.append(photoMessage)
+    }
+    
 }
--- chats-swift-JsqMessages/MessagesViewController.swift
+++ chats-swift-JsqMessages/MessagesViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class MessagesViewController: JSQMessagesViewController {
 
+    var demoData = DemoModelData()
     override func viewDidLoad() {
         //
         super.viewDidLoad()
@@ -19,5 +20,58 @@ class MessagesViewController: JSQMessagesViewController {
         */
         self.senderId = kJSQDemoAvatarIdSquires;
         self.senderDisplayName = kJSQDemoAvatarDisplayNameSquires;
+        
+        
+        self.showLoadEarlierMessagesHeader = true
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage.jsq_defaultTypingIndicatorImage(), style: UIBarButtonItemStyle.Plain, target: self, action: "receiveMessagePressed:")
+         // self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Test", style: UIBarButtonItemStyle.Plain, target: self, action: "receiveMessagePressed:")
+    }
+    // 每行的数据项
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData! {
+        return self.demoData.messages[indexPath.item]
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.demoData.messages.count
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageBubbleImageDataSource! {
+        let msg = self.demoData.messages[indexPath.row]
+        if(msg.senderId == self.senderId){
+            return self.demoData.outgoingBubbleImageData
+        }else{
+            return self.demoData.incomingBubbleImageData
+        }
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageAvatarImageDataSource! {
+        return self.demoData.avatars[self.demoData.messages[indexPath.item].senderId]!
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        var cell = super.collectionView(collectionView, cellForItemAtIndexPath: indexPath) as! JSQMessagesCollectionViewCell
+        var msg = self.demoData.messages[indexPath.row]
+        if(msg.senderId == self.senderId){
+            cell.textView.textColor = UIColor.blackColor()
+        }else{
+            cell.textView.textColor = UIColor.whiteColor()
+        }
+        return cell
+    }
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+      
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    func receiveMessagePressed(sender:UIBarButtonItem)
+    {
+        println("clicked");
     }
 }
